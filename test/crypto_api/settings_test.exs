ae06cc93302ecc8732ceebaafee90232c65c4e65
Renan <renanvy@gmail.com>
2021-06-03 19:31:48 -0300

    Add test to function that update currency value

--- test/crypto_api/settings_test.exs
+++ test/crypto_api/settings_test.exs
@@ -8,4 +8,23 @@ defmodule CryptoApi.SettingsTest do
       assert Settings.list_currencies() == %{"BRL" => 5.4, "CAD" => 1.44, "EUR" => 0.92}
     end
   end
+
+  describe "update_currency/1" do
+    test "update currency when params is valid" do
+      assert {:ok, _currencies} =
+               Settings.update_currency(%{"currency" => "BRL", "value" => 5.22})
+
+      currencies = Settings.list_currencies()
+
+      assert currencies["BRL"] == 5.22
+
+      # Rollback value
+      Settings.update_currency(%{"currency" => "BRL", "value" => 5.4})
+    end
+
+    test "returns error when params is valid" do
+      assert {:error, _changeset} =
+               Settings.update_currency(%{"currency" => "INVALID", "value" => 5.22})
+    end
+  end
 end
